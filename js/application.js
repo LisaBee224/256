9bf901234d3f83627ad5561cb6d35b7fd2c8094f
LisaBee224 <lisakbuch@gmail.com>
2016-06-20 16:18:57 -0400

    Delete comments

--- js/application.js
+++ js/application.js
@@ -5,7 +5,6 @@ game.setUpBoardElements();
 $("body").keydown(function(e){
   game.initBoard= _.clone(game.board)
   if(e.keyCode == 37){
-
     game.moveHorizontal(37);
   }else if(e.keyCode == 39){
     game.moveHorizontal(39);
@@ -26,7 +25,7 @@ var Game = function(){
 
 Game.prototype.makeBoard = function(){
   arr = [0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0];
-arr.sort(function(){
+  arr.sort(function(){
   return .5-Math.random();
 });
  var boardArray = [];
@@ -40,12 +39,11 @@ arr.sort(function(){
 
 Game.prototype.setUpBoardElements= function(){
 var nestedArr = this.board;
-for (var i=0; i < 4; i++){
+for(var i=0; i < 4; i++){
   for(var j =0; j<4; j++){
     cell = $("#row-" + i + " .col-" + j)
     if ((nestedArr[i][j] === 0) || (nestedArr[i][j] === "")){
       cell.text("");
-
       cell.css("background-color", "#d3fc8d");
     }else if(nestedArr[i][j] === 2){
       cell.text(nestedArr[i][j])
@@ -63,25 +61,27 @@ for (var i=0; i < 4; i++){
     }else if(nestedArr[i][j] === 32){
       cell.text(nestedArr[i][j])
       cell.css("background-color", "#74c4db");
-
-  }else if(nestedArr[i][j] === 64){
+    }else if(nestedArr[i][j] === 64){
       cell.text(nestedArr[i][j])
-      cell.css("background-color", "#9ff945");
-
-  }
-  else if(nestedArr[i][j] === 128){
+      cell.css("background-color", "#a6fc94");
+    }else if(nestedArr[i][j] === 128){
       cell.text(nestedArr[i][j])
-      cell.css("background-color", "#cffce6");
-
-  }
-   else if(nestedArr[i][j] === 256{
+      cell.css("background-color", "#71fc6f");
+   }else if(nestedArr[i][j] === 256){
       cell.text(nestedArr[i][j])
-      cell.css("background-color", "#fcb73f");
-
-  }
-    else{
+      cell.css("background-color", "#ffdd66");
+        $("#header").append("<h2>you won!</h2>");
+        $("#header h2").animate({
+            right: '250px',
+            opacity: '0.5',
+            color: '#ff1c60'
+        });
+       setTimeout(function(){
+        location.reload()
+        },2500)
+  }else{
       cell.text(nestedArr[i][j])
-     }
+    }
     }
   }
 }
@@ -118,9 +118,7 @@ Game.prototype.moveVertical=function(direction){
       }
   }
   this.board = _.unzip(this.board);
-
 }
-
 
 
 Game.prototype.addRandomNumber = function(){
@@ -143,61 +141,10 @@ Game.prototype.compareBoards=function(){
     this.addRandomNumber();
   }else{
     this.addRandomNumber();
+    this.isLost();
     this.setUpBoardElements();
   };
 }
-
-// Game.prototype.addRandomNumber = function(){
-//   numsToAdd = [2,4,2,4,2,2,2,2,2,2]
-//   randNum = _.sample(numsToAdd, 1);
-//   var i = _.sample([0,1,2,3]);
-//   var j = _.sample([0,1,2,3]);
-//   while(this.board[i][j] === ""){
-//   if (this.board[i][j] === "") {
-//     this.board[i][j] = randNum
-//   }else{
-//       i = _.sample([0,1,2,3]);
-//       j = _.sample([0,1,2,3]);
-//   }
-// }
-// }
-// Game.prototype.compareBoards=function(){
-//   if( _.join(this.initBoard, ',')) == (_.join(this.board, ',')){
-//     this.setUpBoardElements();
-//   }else{
-//     this.addRandomNumber();
-//     this.setUpBoardElements();
-//   };
-// }
-
-// Game.prototype.printBoard=function(){
-//   var arr=this.board;
-//   for(var i=0;i<4;i++){
-//     var rowId = array[i]
-//     for(var j=0;j<4;j++){
-//       var cell = $("#row-" + i + " .col-" + j)
-//       if (arr[i][j] === 0 ){
-//         cell.text("")
-//       }else{
-//         cell.text(array[i][j]);
-//       }
-//     }
-//   }
-// }
-// Game.prototype.compareBoards=function(){
-//   for (var i=0;i<this.board.length;i++){
-//     var boardRow = this.board[i];
-//     var changedRow = this.changedBoard[i];
-//     for (var j=0;j<this.board.length;j++){
-//       if(boardRow[j] == changedRow[j]){
-//         return false;
-//       }else{
-//         return true;
-//       }
-//     }
-//   }
-// }
-
 
 function addNums(row){
   if (row != []){
@@ -212,72 +159,33 @@ function addNums(row){
   return row;
 }
 
-// Game.prototype.addAdjacentNumbersRight = function(){
-//   var board = this.board;
-//     for(var i =0; i <board.length; i++){
-//       board[i] = _.compact(board[i])
-//       while (board[i].length < 4){
-//         board[i].unshift("");
-//       }
-//   }
-// }
+Game.prototype.allFull=function(){
+  var counter = 0
+  for (var i=0; i<4; i++){
+    if (this.board[i].indexOf("") == -1){
+      counter ++;
+    }
+  }
+  if (counter == 4){
+    return true;
+  }
+}
+Game.prototype.sameNumAdjacents=function(){
+  var counter=0;
+    for (var i=0; i<4; i++){
+      for (var j=0;j<4; j++){
+      if (this.board[i][j] == this.board[i][j+1]){
+        counter++;
+      }
+      }
+    }
+    if (counter > 0){
+      return true;
+    }
+}
 
-
-
-
-// Game.prototype.moveLeft = function(direction){
-
-//   var board=this.board;
-//     for(var i=0; i<board.length; i++){
-//       board[i] = _.compact(board[i])
-//       this.board[i] = addNums(this.board[i]);
-//       while (board[i].length < 4){
-//         board[i].push("");
-//       }
-//   }
-//   this.changedBoard=board;
-//   this.addRandomNumber();
-//   this.setUpBoardElements();
-// }
-
-// Game.prototype.moveRight = function(){
-//   var board=this.board;
-//     for(var i=0; i <board.length; i++){
-//       board[i] = _.compact(board[i])
-//       this.board[i] = addNums(this.board[i]);
-//       while (board[i].length < 4){
-//         board[i].unshift("");
-//       }
-//   }
-//   this.changedBoard=board;
-//   this.addRandomNumber();
-//   this.setUpBoardElements();
-// }
-
-// Game.prototype.moveUp = function(){
-//     this.board = _.unzip(this.board);
-//     for(var i=0; i<this.board.length; i++){
-//       this.board[i]=_.compact(this.board[i])
-//       this.board[i]= addNums(this.board[i]);
-//       while (this.board[i].length < 4){
-//         this.board[i].push("");
-//       }
-//   }
-//   this.board = _.unzip(this.board)
-//   this.addRandomNumber();
-//   this.setUpBoardElements();
-// }
-
-// Game.prototype.moveDown = function(){
-//     this.board = _.unzip(this.board);
-//     for(var i=0; i <this.board.length; i++){
-//       this.board[i] = _.compact(this.board[i])
-//       this.board[i] = addNums(this.board[i]);
-//       while (this.board[i].length < 4){
-//         this.board[i].unshift("");
-//       }
-//   }
-//   this.board = _.unzip(this.board)
-//   this.addRandomNumber();
-//   this.setUpBoardElements();
-// }
+Game.prototype.isLost=function(){
+  if (this.allFull() && !(this.sameNumAdjacents())){
+    $("#header").append("<h2>bummer, you lost!</h2>");
+  }
+}
